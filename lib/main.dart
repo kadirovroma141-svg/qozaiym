62f9c74d85ef04096daff2f42fb2073bfceebbb9
kadirovroma141-svg <kadirovroma141@gmail.com>
2026-02-15 23:27:47 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'NaviBlind',
+      title: 'Qozaiym',
       theme: ThemeData(
         brightness: Brightness.dark,
         scaffoldBackgroundColor: AppTheme.background,
